f3c8ce034a2ee7193a7b17c42a1c33a321499a70
N10T <andy.tisba@gmail.com>
2021-02-07 20:22:06 +0100

    correct the API url

--- api/defaultAPI.js
+++ api/defaultAPI.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: "http://localhost:4000"+"/api",
-  // baseURL: (process.env.REACT_APP_BACKEND_URL || "https://at-portfolio-api.herokuapp.com")+"/api",
+  // baseURL: process.env.REACT_APP_BACKEND_URL ||"http://localhost:4000"+"/api",
+  baseURL: (process.env.REACT_APP_BACKEND_URL || "https://at-portfolio-api.herokuapp.com")+"/api",
 })
 const get = (_) => service.get("/test").then(res=>res.data).catch(err=>console.log(err));
 const getOne = (id) => service.get("/" + id).then(res=>res.data).catch(err=>console.log(err));

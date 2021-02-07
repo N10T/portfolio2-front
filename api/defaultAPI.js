2589693c2e0149bda4d5454da83882cd7e16868a
N10T <andy.tisba@gmail.com>
2021-02-07 13:15:10 +0100

    setup with dotenv

--- api/defaultAPI.js
+++ api/defaultAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const service = axios.create({
   // baseURL: "http://localhost:3080"+"/api",
-  baseURL: process.env.REACT_APP_BACKEND_URL+"/api",
+  baseURL: (process.env.REACT_APP_BACKEND_URL || "https://at-portfolio-api.herokuapp.com")+"/api",
 })
 const get = (_) => service.get("/test").then(res=>res.data).catch(err=>console.log(err));
 const getOne = (id) => service.get("/" + id).then(res=>res.data).catch(err=>console.log(err));
